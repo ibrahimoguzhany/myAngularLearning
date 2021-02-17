a5949af69fd9c71180eb371b48135cde7ba67c49
İbrahim Oğuzhan YILMAZ <ibrahimoguzhany@gmail.com>
2021-02-17 18:46:58 +0300

    first

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
